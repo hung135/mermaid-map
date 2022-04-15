482e2ae63415a3bdf2a9e4763143b16525eb3374
hung135 <hung135@hotmail.com>
2022-04-15 12:35:25 +0000

    saving work

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 test_py:
 	python /workspaces/mermaid-map/src/mermaid_mapper/py/py.py 
 	python /workspaces/mermaid-map/src/mermaid_mapper/json/jsonParser.py /workspaces/mermaid-map/test.json
+ 
+	
--- src/mermaid_mapper/json/jsonParser.py
+++ src/mermaid_mapper/json/jsonParser.py
@@ -5,8 +5,34 @@ import random
 ignore_list=[ 
             "end_col_offset",
             "end_lineno", 
-            "lineno","col_offset","end_col_offset"
+            "lineno","col_offset","end_col_offset","ctx","id"
                         ]
+def get_paths(d, root="obj"):
+    def recur(d):
+        if isinstance(d, dict):
+            for key, value in d.items():
+                yield f'.{key}'
+                yield from (f'.{key}{p}' for p in get_paths(value))
+
+        elif isinstance(d, list):
+            for i, value in enumerate(d):
+                yield f'[{i}]'
+                yield from (f'[{i}]{p}' for p in get_paths(value))
+
+    return (root + p for p in recur(d))
+def get_json_path(json_string):
+    import json
+
+    from jsonpath_ng import jsonpath, parse
+ 
+    json_data = json.loads(json_string)
+
+    jsonpath_expression = parse('$.body[0].names')
+
+    match = jsonpath_expression.find(json_data)
+
+    #print(match)
+    print("value is", match[0].value)
 
 def traverseJson(data, mermaidOutput, parent):
     if isinstance(data, dict):
@@ -43,20 +69,25 @@ def traverseJson(data, mermaidOutput, parent):
                 mermaidOutput.append("{0}-->{1}\n".format(valId, id))
     return 0
 
-def main(filename):
+def main(filename): 
     mermaidOutput = ['graph TD\n']
-
+    data=None
     with open(filename) as jsonInput:
         data = json.load(jsonInput)
         fileId = str(random.randint(0, 10000))
         mermaidOutput.append("{0}[\"{1}\"]\n".format(fileId, filename))
         traverseJson(data, mermaidOutput, (filename, fileId))
+        get_json_path(json.dumps(data)) 
 
     with open("output.md", "w") as jsonOutput:
         jsonOutput.writelines("```mermaid\n")
         
         jsonOutput.writelines(mermaidOutput)
         jsonOutput.writelines("\n```\n")
+ 
+        
+     
+    
     return 0
 
 if __name__ == "__main__":
