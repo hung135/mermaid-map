1fbd069dbdb77607241ebbb569e8164bd116e2d4
Vu Nguyen <vu.nguyen3950@gmail.com>
2022-04-21 09:46:00 -0400

    added docstring to functions

--- src/mermaid_mapper/json_to_mermaid/k8s_parser.py
+++ src/mermaid_mapper/json_to_mermaid/k8s_parser.py
@@ -12,6 +12,7 @@ class K8SParser:
         self.load_yaml()
     
     def load_yaml(self) -> None:
+        """Loads the yaml files and makes a parser for each particular K8S component (i.e. deployment)"""
         for path in self.yaml_file_paths:
             with open(path, 'r') as yaml_file, open("output.json", 'w') as json_file:
                 yaml_contents = yaml.safe_load(yaml_file)
@@ -24,6 +25,7 @@ class K8SParser:
                 self.service_parser = parsed_json
     
     def setup_deployment_visualization(self) -> None:
+        """Generate mermaid code for the deployment section of the diagram based on the parsed deployment yaml"""
         pod_count = int(self.deployment_parser.json["spec"]["replicas"])
         pod_container_image = self.deployment_parser.json["spec"]["template"]["spec"]["containers"][0]["image"]
         container_list = self.deployment_parser.json["spec"]["template"]["spec"]["containers"]
@@ -40,6 +42,7 @@ class K8SParser:
             self.pod_list[pod_id] = (self.deployment_parser.json["spec"]["selector"]["matchLabels"],)
 
     def setup_service_visualization(self) -> None:
+        """Generate mermaid code for the service section of the diagram based on the parsed service yaml"""
         service_name = self.service_parser.json["metadata"]["name"]
         selector_label = self.service_parser.json["spec"]["selector"]
         service_target_port = self.service_parser.json["spec"]["ports"][0]["targetPort"]
@@ -51,6 +54,7 @@ class K8SParser:
                 self.service_parser.mermaid.append(f'{key}-->|"target port: {service_target_port}"|{service_id}\n')
 
     def create_mermaid(self) -> None:
+        """Output the generated mermaid code into a mermaid file"""
         with open('output.md', 'w') as mermaid_output:
             mermaid_output.write('```mermaid\ngraph TD\nsubgraph Node\n')
             self.cleanup_parsers_mermaid()
@@ -63,6 +67,7 @@ class K8SParser:
             mermaid_output.write('end\n```')
 
     def cleanup_parsers_mermaid(self) -> None:
+        """Remove the unnecessary filename box from the mermaid output"""
         if self.service_parser != None:
             self.service_parser.mermaid.pop(0)
             self.service_parser.mermaid.pop(0)
